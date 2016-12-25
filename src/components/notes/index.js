e0fa745e50f15b770391a0350e68265e377620b1
Zaicheng Qi <vmlinz@gmail.com>
2016-12-25 17:49:50 +0800

    finished loading and displaying notes

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,16 +1,24 @@
 import React, {PropTypes} from 'react';
 import { connect } from 'react-firebase';
 
+import NotesList from './notes-list';
+
 class Notes extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    console.log(this.props);
-    return (<div>
-      <h2>Notes: {JSON.stringify(this.props.notes)}</h2>
-    </div>);
+    if(this.props.notes) {
+      return (<div>
+        <h2>Notes for {this.props.username}</h2>
+        <NotesList notes={this.props.notes}/>
+      </div>);
+    } else {
+      return (<div>
+        <h2>Notes(loading)</h2>
+      </div>);
+    }
   }
 }
 
